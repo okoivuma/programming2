574ec8a308aa2f17cc86d29eedcd91865b3c39d7
Oskari Koivumäki <oskari.koivumaki@student.oulu.fi>
2024-02-19 15:49:50 +0000

    ConstantValues.java uploaded

---
+++ homework1/src/main/java/dev/m3s/programming2/homework1/ConstantValues.java
@@ -0,0 +1,18 @@
+package dev.m3s.programming2.homework1;
+
+import java.time.Year;
+
+public class ConstantValues {
+    public static final String NO_NAME = "No name";
+    public static final String NO_TITLE= "No title";
+    public static final String NO_BIRTHDATE = "Not available";
+    public static final String INVALID_BIRTHDAY = "Invalid birthday!";
+    public static final String INCORRECT_CHECKMARK = "Incorrect check mark!";
+    public static final int MIN_ID = 1;
+    public static final int MAX_ID = 100;
+    public static final double MIN_CREDITS = 0.0;
+    public static final double MAX_CREDITS = 300.0;
+    public static final double BACHELOR_CREDITS = 180.0;
+    public static final double MASTER_CREDITS = 120.0;
+    public static final int CURRENT_YEAR = Year.now().getValue();
+}
