e39fe8b5ccffc3abafb6b3b995379916a526bdbe
Oskari Koivumäki <oskari.koivumaki@student.oulu.fi>
2024-04-03 12:55:05 +0000

    Upload New File

---
+++ homework3/src/main/java/dev/m3s/programming2/homework3/AssistantTeacher.java
@@ -0,0 +1,42 @@
+//package kotiteht3;
+package dev.m3s.programming2.homework3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssistantTeacher extends Employee implements Teacher, Payment {
+
+    private List<DesignatedCourse> courses = new ArrayList<>();
+
+    public AssistantTeacher(String lname, String fname){
+        super(lname,fname);
+    }
+
+    public String getEmployeeIdString(){
+        return "OY_ASSISTANT";
+    }
+
+    public String getCourses(){
+        String coursesString = "";
+        for (DesignatedCourse course : courses){
+            coursesString += course + "\n";
+        }
+        return coursesString;
+    }
+
+    public void setCourses(List<DesignatedCourse> courses) {
+        if (courses != null){
+            this.courses = courses;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return  "Teacher id: " + getIdString() + "\n" +
+                "        First name: " + getFirstName() + ", Last name: " + getLastName() + "\n" +
+                "        Birthdate: " + getBirthDate() + "\n" +
+                "        Salary: " + String.format("%.2f", calculatePayment()) + "\n" +
+                "        Assistant for courses:\n" +
+                "       " + getCourses();
+    }
+}
