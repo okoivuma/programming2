5bd6b5b66029055b5aeb4aab39cd08c9c2f28f1b
Oskari Koivumäki <oskari.koivumaki@student.oulu.fi>
2024-04-03 12:58:09 +0000

    Upload New File

---
+++ homework3/src/main/java/dev/m3s/programming2/homework3/StudentCourse.java
@@ -0,0 +1,93 @@
+package dev.m3s.programming2.homework3;
+//package kotiteht3;
+
+
+
+
+public class StudentCourse {
+
+    private Course course;
+    private int gradeNum;
+    private int yearCompleted;
+
+    public StudentCourse(){
+
+    }
+
+    public StudentCourse(Course course, final int gradeNum, final int yearCompleted){
+        setCourse(course);
+        setGrade(gradeNum);
+        setYear(yearCompleted);
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public int getGradeNum() {
+        return gradeNum;
+    }
+
+    protected void setGrade(int gradeNum){
+        if (course.isNumericGrade() && checkGradeValidity(gradeNum)){
+            this.gradeNum = gradeNum;
+        } else if (checkGradeValidity(gradeNum)) {
+            this.gradeNum = gradeNum;
+        }
+
+        if (yearCompleted == 0){
+            yearCompleted = ConstantValues.CURRENT_YEAR;
+        }
+    }
+
+    private boolean checkGradeValidity(final int gradeNum){
+        if (course.isNumericGrade()){
+            return gradeNum >= 0 && gradeNum <= 5;
+        } else {
+            char gradeChar = Character.toUpperCase((char) gradeNum);
+            return gradeChar == 'F' || gradeChar == 'A';
+        }
+    }
+
+    public boolean isPassed(){
+       /*
+        if (course.isNumericGrade()){
+            return gradeNum > 0 && gradeNum <= 5;
+        } else {
+            char gradeChar = Character.toUpperCase((char) gradeNum);
+            return gradeChar == 'A' || gradeChar == 'a';
+        }*/
+        return gradeNum > ConstantValues.MIN_GRADE && gradeNum != ConstantValues.GRADE_FAILED;
+    }
+
+    public int getYear(){
+        return yearCompleted;
+    }
+
+    public void setYear(final int year){
+        if (year > 2000 && year <= ConstantValues.CURRENT_YEAR){
+            yearCompleted = year;
+        }
+    }
+    private String printGrade(){
+        if (checkGradeValidity(gradeNum)) {
+            if (gradeNum == 0) {
+                return "Not graded";
+            } else if (course.isNumericGrade()) {
+                return String.valueOf(gradeNum);
+            } else {
+                return String.valueOf((char) gradeNum);
+            }
+        }
+        return "";
+    }
+
+    @Override
+    public String toString(){
+        return course + " Year: " + yearCompleted + ", Grade: " + printGrade() + ".]";
+    }
+}
