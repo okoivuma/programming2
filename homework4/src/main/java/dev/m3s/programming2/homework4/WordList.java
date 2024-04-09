292931bd9915a9e038893fd88b8005ce5e081bb0
Oskari Koivumäki <oskari.koivumaki@student.oulu.fi>
2024-04-09 15:16:43 +0000

    Upload New File

---
+++ homework4/src/main/java/dev/m3s/programming2/homework4/WordList.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class WordList {
+    private List<String> words = new ArrayList<>();
+
+    public WordList(String filename){
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String row;
+            while ((row = reader.readLine()) != null) {
+                words.add(row.toLowerCase());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Unable to read file");
+        }
+    }
+
+    public List<String> giveWords(){
+        return words;
+    }
+}
