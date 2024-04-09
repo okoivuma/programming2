cf65f462c233fcc479629d6b46cf9a960026849f
Oskari Koivumäki <oskari.koivumaki@student.oulu.fi>
2024-04-09 15:49:43 +0000

    Replace Main.java

--- homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
+++ homework4/src/main/java/dev/m3s/programming2/homework4/Main.java
@@ -1,43 +1,48 @@
-package dev.m3s.programming2.homework4;
+import java.io.IOException;
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        WordList words = new WordList("words.txt");
-        Hangman newgame = new Hangman(words, 5);
+        try {
+            WordList words = new WordList("words.txt");
+            Hangman newgame = new Hangman(words, 5);
 
 
-        System.out.println("Welcome to play Hangman!");
+            System.out.println("Welcome to play Hangman!");
 
-        while (!newgame.theEnd()){
+            while (!newgame.theEnd()) {
 
-            System.out.println("The hidden word...");
-            System.out.println(newgame.getMaskedWord());
+                System.out.println("The hidden word...");
+                System.out.println(newgame.getMaskedWord());
 
-            System.out.println("Guesses left: " + newgame.guessesLeft());
-            System.out.println("Guessed letters: " + newgame.guesses() + "\n");
+                System.out.println("Guesses left: " + newgame.guessesLeft());
+                System.out.println("Guessed letters: " + newgame.guesses() + "\n");
 
-            System.out.println("Guess a letter: ");
+                System.out.println("Guess a letter: ");
 
-            String input = scanner.next();
-            if (input.length() > 1){
-                System.out.println("You have to give only on character");
-            } else {
-                Character c = Character.toLowerCase(input.charAt(0));
-                newgame.guess(c);
+                String input = scanner.next();
+                if (input.length() > 1) {
+                    System.out.println("You have to give only on character");
+                } else {
+                    Character c = Character.toLowerCase(input.charAt(0));
+                    newgame.guess(c);
+                }
+
+                System.out.println("\n");
             }
 
-            System.out.println("\n");
+            if (newgame.victory()) {
+                System.out.println("Congratulations! You won!!!");
+                System.out.println("The hidden word was: " + newgame.word());
+            } else {
+                System.out.println("Sorry, you lost!");
+                System.out.println("The hidden word was: " + newgame.word());
+            }
+        } catch (IOException e) {
+            System.out.println("Error while reading the wordlist");
+        } finally {
+            scanner.close();
         }
-
-        if (newgame.victory()){
-            System.out.println("Congratulations! You won!!!");
-            System.out.println("The hidden word was: " + newgame.word());
-        } else {
-            System.out.println("Sorry, you lost!");
-            System.out.println("The hidden word was: " + newgame.word());
-        }
-        scanner.close();
     }
 }
